e4890341f23ced207dbb200e931fc2c5b7bd22a7
Amélie Beirens <amelie.beirens@etudiant.univ-reims.fr>
2020-02-14 09:08:18 +0100

    avance : commit page connexion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,24 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  // {
+  //   path: '',
+  //   loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  // },
   {
-    path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    path:'',
+    redirectTo: 'connexion',
+    pathMatch:'full'
+  },
+  {
+    path: 'connexion',
+    loadChildren: () => import('./connexion/connexion.module').then( m => m.ConnexionPageModule)
   },
   {
     path: 'detail-film',
     loadChildren: () => import('./detail-film/detail-film.module').then( m => m.DetailFilmPageModule)
-  },  {
+  },
+  {
     path: 'tab4',
     loadChildren: () => import('./tab4/tab4.module').then( m => m.Tab4PageModule)
   },
@@ -28,6 +38,14 @@ const routes: Routes = [
   {
     path: 'aide',
     loadChildren: () => import('./aide/aide.module').then( m => m.AidePageModule)
+  },
+  {
+    path: 'ma-list',
+    loadChildren: () => import('./ma-list/ma-list.module').then( m => m.MaListPageModule)
+  },
+  {
+    path: 'connexion',
+    loadChildren: () => import('./connexion/connexion.module').then( m => m.ConnexionPageModule)
   }
 
 
