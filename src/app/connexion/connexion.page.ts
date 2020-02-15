4e7e371b39456b4bad9361701e60b0323f576b5e
Oriane <oriane.martinet@gmail.com>
2020-02-15 22:30:23 +0100

    feat: add relation between users and movies

--- src/app/connexion/connexion.page.ts
+++ src/app/connexion/connexion.page.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import {ModalController} from "@ionic/angular";
+import {AngularFireDatabase} from "@angular/fire/database";
+import {Router} from "@angular/router";
+import {Tab1Page} from "../tab1/tab1.page";
 
 @Component({
   selector: 'app-connexion',
   templateUrl: './connexion.page.html',
   styleUrls: ['./connexion.page.scss'],
 })
-export class ConnexionPage implements OnInit {
+export class ConnexionPage{
+  public users : any;
+  constructor(public modalController : ModalController, public afDB: AngularFireDatabase, public router: Router) {
+    this.afDB.list('/Users').valueChanges().subscribe((data)=> {
+      this.users = data;
+    });
+  }
 
-  constructor() { }
-
-  ngOnInit() {
+  connect(user) {
+    console.log(user);
+    this.router.navigate(['tab1'], {
+      queryParams: user
+    })
   }
 
 }
