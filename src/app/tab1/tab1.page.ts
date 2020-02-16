1048d13e2fea0ca966a9545e09a0b1227e6e92c6
Oriane <oriane.martinet@gmail.com>
2020-02-16 15:18:25 +0100

    feat: add film look datas

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-
+import {Router} from "@angular/router";
 import {ModalController} from '@ionic/angular';
 import {DetailFilmPage} from '../detail-film/detail-film.page';
 import {AngularFireDatabase} from '@angular/fire/database';
@@ -14,8 +14,10 @@ export class Tab1Page {
     public movies: any;
     public moviesList: any;
     public user: any;
+    afficheImage: any;
+    affiche: any;
 
-    constructor(public modalController: ModalController, public afDB: AngularFireDatabase, public activatedRoute: ActivatedRoute) {
+    constructor(public modalController: ModalController, public afDB: AngularFireDatabase, public activatedRoute: ActivatedRoute, public router: Router) {
         this.activatedRoute.queryParams.subscribe((res) => {
             this.user = res;
         });
@@ -23,15 +25,21 @@ export class Tab1Page {
             var movs = [];
             var movList = [];
             var usr = this.user;
+            var affch = this.affiche;
             data.forEach(function (movie) {
                 if (usr.list.indexOf(movie.id) !== -1) {
                     movList.push(movie);
                 } else {
                     movs.push(movie);
                 }
+                if(movie.affiche) {
+                    affch = movie;
+                }
             });
             this.movies = movs;
             this.moviesList = movList;
+            this.afficheImage = affch.affiche;
+            this.affiche = affch;
         });
     }
 
@@ -43,10 +51,9 @@ export class Tab1Page {
     }
 
     async openDetailFilm() {
-        const modal = await this.modalController.create({
-            component: DetailFilmPage
-        });
-        return await modal.present();
+        this.router.navigate(['detail-film'], {
+            queryParams: {movie : this.affiche.id, user : this.user.name}
+        })
     }
 
 }
